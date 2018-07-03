e523194af99f6c535a2c1aaeb19dc1da2a8e8fbb
Patrick Ethier <nuttybrewer@gmail.com>
2018-07-03 15:22:21 +0200

    Fix comments for JavaDoc

--- fieldextraction-morphlines/src/main/java/com/secureops/fieldextraction/morphlines/MorphlinesFieldExtractorItemConfigLoader.java
+++ fieldextraction-morphlines/src/main/java/com/secureops/fieldextraction/morphlines/MorphlinesFieldExtractorItemConfigLoader.java
@@ -33,7 +33,7 @@ public class MorphlinesFieldExtractorItemConfigLoader implements
     private static boolean MORPHLINES_TEST_ON_LOAD_DEFAULT = false;
     private static String MORPHLINES_CONFIG_QUICKCHECK_DEFAULT = ".*";
 
-    
+
     private String section = null;
     public String typeString() {
         return MORPHLINES_TYPE_STRING_KEY;
@@ -45,19 +45,20 @@ public class MorphlinesFieldExtractorItemConfigLoader implements
 
     /**
      * This function will load retrieve the contents of the morphlines config file
-     * 
+     *
      * If the configuration library supports remote filesystems, it will copy the
      * Morphline configuration file locally as well as inspect the contents for all
      * the "definititionFile" directives in grok commands and copy those locally as
      * well.
-     * 
-     * @param config
+     *
+     * @param config Java Configuration object
+     * @param section Java Configuration object section to load
      * @return a String containing the contents of the morphlines config file
-     * @throws ConfigurationException 
-     * @throws IOException 
-     * @throws DecoderException 
-     * @throws Exception 
+     * @throws ConfigurationException when it can't resolve a proper morphline object
+     * @throws IOException when a morphline object in a text file cannot be loaded
+     * @throws DecoderException when a morphline object cannot be parsed
      */
+
     public static String getMorphlineConfigString(Configuration config, String section) throws ConfigurationException, IOException, DecoderException {
 		String morphlinesConfigString = null;
 		if(config.containsKey(MORPHLINES_CONFIG_STRING_KEY)) {
@@ -67,7 +68,7 @@ public class MorphlinesFieldExtractorItemConfigLoader implements
 			morphlinesConfigString = new String(Hex.decodeHex(config.getString(MORPHLINES_CONFIG_ENCODED_KEY).toCharArray()));
 		}
 		else if(config.containsKey(MORPHLINES_CONFIG_FILE_KEY)) {
-			String morphlinesConfigFileString = config.getString(MORPHLINES_CONFIG_FILE_KEY);	
+			String morphlinesConfigFileString = config.getString(MORPHLINES_CONFIG_FILE_KEY);
 	        LOG.info("Loading morphlines configuration file via: "
 	                + morphlinesConfigFileString);
 	        try {
@@ -78,14 +79,14 @@ public class MorphlinesFieldExtractorItemConfigLoader implements
 	        	throw new IOException("Unable to retrieve morphline file " + morphlinesConfigFileString + ": " + e.getMessage());
 	        }
 		}
- 
+
 
 		if(morphlinesConfigString == null || morphlinesConfigString.isEmpty()) {
 			throw new ConfigurationException("Unable to load Morphline string, it is either emtpy or null");
 		}
 		return morphlinesConfigString;
     }
-    
+
     @Override
     public IFieldExtractorItem loadItemConfiguration(Configuration config,
             String section) throws IOException, ConfigurationException{
@@ -108,7 +109,7 @@ public class MorphlinesFieldExtractorItemConfigLoader implements
 		catch (Exception e) {
 			throw new ConfigurationException("Unable to load morphline string : " + morphlinesConfigString + ": " + e.getMessage());
 		}
-		
+
 		com.typesafe.config.Config kiteConf = null;
 		try {
 			kiteConf = typesafeConf.resolve();
@@ -119,7 +120,7 @@ public class MorphlinesFieldExtractorItemConfigLoader implements
 		List<? extends Config> mls = kiteConf.getConfigList("morphlines");
 
         MorphlinesFieldExtractorItem morphlinesItem = new MorphlinesFieldExtractorItem();
-		
+
 		for(Config ml: mls) {
 				if(ml.hasPath("id")) {
 		        	String id = ml.getString("id");
@@ -134,12 +135,12 @@ public class MorphlinesFieldExtractorItemConfigLoader implements
 			        catch(Exception e) {
 			        	throw new ConfigurationException("Unable to compile morphline for section " + ml.getString("id"));
 			        }
-				}				
+				}
 		}
-		
+
 		// Store the config text since it's the only way to differentiate between two ExtractorItems
 		morphlinesItem.setConfigText(kiteConf.root().render(ConfigRenderOptions.concise()));
-        
+
         // We can test the morphline to see if it matches
         if(config.getBoolean(MORPHLINES_TEST_ON_LOAD_KEY, MORPHLINES_TEST_ON_LOAD_DEFAULT)){
         	String testString = config.getString(MORPHLINES_TEST_STRING_KEY);
@@ -147,15 +148,15 @@ public class MorphlinesFieldExtractorItemConfigLoader implements
         		LOG.error("ExtractorItem " + this.section + " requires to test the item but doesn't have a " + MORPHLINES_TEST_STRING_KEY + " set.");
         		throw new ConfigurationException("ExtractorItem " + section + " requires to test the item but doesn't have a " + MORPHLINES_TEST_STRING_KEY + " set.");
         	}
-        	
+
         	ExtractorResult match = morphlinesItem.extract(testString);
-        	
+
         	if(match == null || match.getMatches().isEmpty()) {
         		LOG.error("Test String: " + testString + " | doesn't match morphlines in  " + morphlinesConfigString);
         		throw new ConfigurationException("Test String: " + testString + " | doesn't morphlines in " + morphlinesConfigString);
         	}
         }
-        
+
         morphlinesItem.setPattern(config.getString(FieldExtractorConfigLoader.EXTRACTOR_ITEM_QUICKCHECK_KEY, MORPHLINES_CONFIG_QUICKCHECK_DEFAULT));
         return morphlinesItem;
     }
@@ -173,12 +174,12 @@ public class MorphlinesFieldExtractorItemConfigLoader implements
 				config.clearProperty(MORPHLINES_CONFIG_FILE_KEY);
 			}
 			else {
-				LOG.error("Morphline defined in extractor " + section + " must include " 
+				LOG.error("Morphline defined in extractor " + section + " must include "
 					       + MORPHLINES_CONFIG_FILE_KEY + " if " + MORPHLINES_CONFIG_STRING_KEY + " is not present");
-				throw new ConfigurationException("Morphline defined in extractor " + section + " must include " 
+				throw new ConfigurationException("Morphline defined in extractor " + section + " must include "
 			       + MORPHLINES_CONFIG_FILE_KEY + " if " + MORPHLINES_CONFIG_STRING_KEY + " is not present");
 			}
 		}
-		
+
 	}
 }
